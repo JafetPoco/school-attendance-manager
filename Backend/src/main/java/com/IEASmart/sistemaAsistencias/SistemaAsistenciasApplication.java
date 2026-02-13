f247ee58394ac789c4fadf55cae14086315c3fa0
Jafet Poco Chire <jpococ@unsa.edu.pe>
2026-02-12 19:25:17 -0500

    Subiendo estructura del proyecto

---
+++ Backend/src/main/java/com/IEASmart/sistemaAsistencias/SistemaAsistenciasApplication.java
@@ -0,0 +1,13 @@
+package com.IEASmart.sistemaAsistencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAsistenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAsistenciasApplication.class, args);
+	}
+
+}
