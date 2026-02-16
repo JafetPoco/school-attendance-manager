30ad4bd68b6e5f94bc89efe5bcaeca2bc2ad6549
Jafet Poco Chire <jpococ@unsa.edu.pe>
2026-02-15 19:12:40 -0500

    Diseño dashboard principal

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Login from '../views/Login.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'login',
       component: Login,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
     }
   ],
 })
