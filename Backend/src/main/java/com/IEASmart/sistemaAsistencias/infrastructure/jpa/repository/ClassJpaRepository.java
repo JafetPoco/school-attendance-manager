abf694b1d9f2da5b6ba65f23d17a04dc5e9df63d
Jafet Poco Chire <jpococ@unsa.edu.pe>
2026-04-30 19:19:16 -0500

    Agregando consultas a ClassEntity

--- Backend/src/main/java/com/IEASmart/sistemaAsistencias/infrastructure/jpa/repository/ClassJpaRepository.java
+++ Backend/src/main/java/com/IEASmart/sistemaAsistencias/infrastructure/jpa/repository/ClassJpaRepository.java
@@ -1,11 +1,20 @@
 package com.IEASmart.sistemaAsistencias.infrastructure.jpa.repository;
 
+import com.IEASmart.sistemaAsistencias.domain.model.valueObject.Grade;
+import com.IEASmart.sistemaAsistencias.domain.model.valueObject.Level;
 import com.IEASmart.sistemaAsistencias.domain.model.valueObject.School;
 import com.IEASmart.sistemaAsistencias.infrastructure.jpa.entity.ClassEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ClassJpaRepository extends JpaRepository<ClassEntity, Long> {
     List<ClassEntity> findAllBySchool(School school);
+    Optional<ClassEntity> findBySectionAndGradeAndLevelAndSchool(String section, Grade grade, Level level,School school);
+
+    @Query("SELECT c.id FROM ClassEntity c WHERE c.school = :school")
+    List<Long> findAllIdsBySchool(@Param("school") School school);
 }
--- Backend/src/main/java/com/IEASmart/sistemaAsistencias/infrastructure/jpa/repository/ClassRepositoryImpl.java
+++ Backend/src/main/java/com/IEASmart/sistemaAsistencias/infrastructure/jpa/repository/ClassRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.IEASmart.sistemaAsistencias.infrastructure.jpa.repository;
 
 import com.IEASmart.sistemaAsistencias.domain.model.Class;
+import com.IEASmart.sistemaAsistencias.domain.model.valueObject.Grade;
+import com.IEASmart.sistemaAsistencias.domain.model.valueObject.Level;
 import com.IEASmart.sistemaAsistencias.domain.model.valueObject.School;
 import com.IEASmart.sistemaAsistencias.domain.repository.ClassRepository;
 import com.IEASmart.sistemaAsistencias.infrastructure.mapper.ClassMapper;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Repository
@@ -35,5 +38,19 @@ public class ClassRepositoryImpl implements ClassRepository {
     @Override
     public Class save(Class newClass) {
         return mapper.toDomain(classJpaRepository.save(mapper.toEntity(newClass)));
+    }
+
+    @Override
+    public List<Long> findAllIdsBySchool(School school) {
+        if (Objects.isNull(school)) {
+            return List.of();
+        }
+        return classJpaRepository.findAllIdsBySchool(school).stream().toList();
+    }
+
+    @Override
+    public Optional<Class> findByClassInformation(String section, Grade grade, Level level, School school) {
+        return classJpaRepository.findBySectionAndGradeAndLevelAndSchool(section, grade, level, school)
+                .map(mapper::toDomain);
     }
 }
