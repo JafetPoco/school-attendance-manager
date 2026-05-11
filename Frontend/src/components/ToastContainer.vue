0686e434cb2a801a59e5fb3a83a1b230d885603c
Jafet Poco Chire <jpococ@unsa.edu.pe>
2026-05-11 11:47:35 -0500

    release: v1.1.2

--- Frontend/src/components/ToastContainer.vue
+++ Frontend/src/components/ToastContainer.vue
@@ -2,7 +2,8 @@
   <transition-group name="toast" tag="div" class="fixed bottom-4 right-4 z-50 space-y-2">
     <div v-for="toast in toasts" 
          :key="toast.id"
-         class="flex items-center space-x-3 px-4 py-3 rounded-lg shadow-lg min-w-75 max-w-md bg-slate-900/90 backdrop-blur-md border border-slate-600 shadow-slate-300 text-white">
+         class="flex items-center space-x-3 px-4 py-3 rounded-lg shadow-lg min-w-75 max-w-md shadow-slate-300 text-white"
+         :class="styleByType(toast.type)">
       <component :is="toast.icon" class="w-5 h-5 shrink-0" />
       <div class="flex-1">
         <p class="text-sm font-medium">{{ toast.title }}</p>
@@ -26,6 +27,21 @@ const toasts = computed(() => toastStore.toasts)
 
 const removeToast = (id: number) => {
   toastStore.removeToast(id)
+}
+
+const styleByType = (type: string) => {
+  switch (type) {
+    case 'success':
+      return 'bg-green-600/90 border-green-600'
+    case 'error':
+      return 'bg-red-600/90 border-red-600'
+    case 'info':
+      return 'bg-blue-600/90 border-blue-600'
+    case 'warning':
+      return 'bg-yellow-600/90 border-yellow-600'
+    default:
+      return 'bg-gray-600/90 border-gray-600'
+  }
 }
 </script>
 
